0c7aa4f57381a7f7ff743a227adfa11b8db4eb1c
1432junaid <1432junaid@gmail.com>
2020-11-17 19:03:43 +0530

    dfs_bfs on undirected weighted graph

--- graph/dfs_bfs.cpp
+++ graph/dfs_bfs.cpp
@@ -1,17 +1,62 @@
 #include <iostream>
 #include <vector>
 #include <map>
-
+#include <queue>
 using namespace std;
 
 void dfs(int);
+void bfs(int);
+
 vector<pair<int,int>> graph[6];
+queue<int> q;
+bool visited[6];
+int dist[6];
 
 int main(){
-	graph[1].insert({5,1});	//connected node,weight
-	graph[1].insert({4,9});
-	graph[1].insert({2,5});
-	graph[2].insert({1,5});
-	graph[2].insert({3,2});
-	graph[3].insert({5,1});
+	graph[1].push_back({2,5});	//node,weight
+	graph[1].push_back({4,9});
+	graph[1].push_back({5,1});
+
+	graph[2].push_back({1,5});
+	graph[2].push_back({3,2});
+
+	graph[3].push_back({2,2});
+	graph[3].push_back({4,7});
+
+	graph[4].push_back({3,7});
+	graph[4].push_back({1,9});
+	graph[4].push_back({5,2});
+
+	graph[5].push_back({4,2});
+	graph[5].push_back({1,1});
+//	dfs(5);
+	bfs(1);
+	return 0;
 }
+
+void dfs(int node){
+	if(visited[node]) return;
+	visited[node] = true;
+	cout << node << "  visited " << endl;
+//	vector<pair<int,int>> :: iterator itr = graph.begin();
+	for(auto itr : graph[node]) dfs(itr.first);
+}
+
+void bfs(int node){
+	visited[node] = true;
+	dist[node] = 0;
+	q.push(node);
+	while(!q.empty()){
+		int s = q.front();
+                q.pop();
+                cout << s << "  visited " << endl;
+                for(auto itr : graph[s]){
+                        if(visited[itr.first])continue;
+                        visited[itr.first] = true;
+                        dist[itr.first] = dist[s]+1;
+                        q.push(itr.first);
+                }
+        }
+}
+
+
